1ef29b623c7e0a57083d189ea9b76659e0602d83
Kiara980 <kiaradb510@gmail.com>
2024-04-24 17:45:16 -0400

    Hello there :)

--- vscode/Chapter01-vscode/HelloCS/Program.cs
+++ vscode/Chapter01-vscode/HelloCS/Program.cs
@@ -1,4 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, C#!");
-throw new Exception();
+Console.WriteLine("My name is Kiara Bowen");
+Console.WriteLine("Version: 8.0.204");
+//throw new Exception();
 int z;
